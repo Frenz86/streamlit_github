bf0eaa6533f4054a68f6c569482e6e905be46e42
Dani <Frenz86@gmail.com>
2021-01-17 15:35:00 +0100

    ww

---
+++ run.py
@@ -0,0 +1,4 @@
+import subprocess
+
+#link = 'https://raw.githubusercontent.com/Frenz86/streamlit_github/main/CaliforniaHousingPrices.py'
+subprocess.call('streamlit run https://raw.githubusercontent.com/Frenz86/streamlit_github/main/CaliforniaHousingPrices.py ')
